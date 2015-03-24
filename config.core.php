4ff2dea59f74fbfe3ae06fa31b4cb54088751a2b
AviZeltser <avi.zeltser@gmail.com>
2015-03-24 12:36:25 +0600

    Tourpage

---
+++ config.core.php
@@ -0,0 +1,4 @@
+<?php
+define('MODX_CORE_PATH', '/home/u166930/bm-technologyru/www/dev/core/');
+define('MODX_CONFIG_KEY', 'config');
+?>
--- js/main.js
+++ js/main.js
@@ -4,10 +4,32 @@
 
 $(document).ready(function() {
     initMainSlider();
+    initTabs();
 })
 
 function initMainSlider() {
     $('.js-promo-slides').flexslider({
         animation: "slide"
     });
+}
+
+function initTabs() {
+    $('.b-tab__item').on('click', function() {
+        $('.b-tab__item').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+        var iindex = $(this).data('index');
+        $('.b-tabs__body').each(function () {
+            $(this).removeClass('visible');
+            if($(this).data('parent') == iindex) {
+                $(this).addClass('visible');
+            }
+        })
+        console.log($(this).data('index'));
+    })
+}
+
+function stub() {
+
 }
